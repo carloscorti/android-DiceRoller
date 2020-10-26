c48e24a8483ca2910814926376bd70a5c06fb311
Carlos <carlosacorti@hotmail.com>
2020-10-26 17:07:30 +0100

    setting up button listener

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.example.diceroller.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(R.layout.activity_main)
+//        val rollButton : Button = findViewById(R.id.roll_button)
+        val view = binding.root
+        setContentView(view)
+
+        val rollButton = binding.rollButton
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "You pressed the button", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
